7f39080d3227829e16cb1d7f0e63eb77d290d38f
Nick Nyanjui <nicknyanjui@gmail.com>
2023-03-03 02:32:46 +0300

    Add 3-app.py

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -8,29 +8,35 @@ app = Flask(__name__)
 
 babel = Babel(app=app)
 
+
 class Config(object):
     """Configuration for babel"""
     LANGUAGES = ["en", "fr"]
     TIMEZONE = "UTC"
 
+
 config = Config()
+
 
 @babel.localeselector
 def get_locale():
     """Returns best matching locale according to language weights"""
     return request.accept_languages.best_match(config.LANGUAGES)
 
+
 @babel.timezoneselector
 def get_timezone():
     """Sets the timezone for the app"""
     return config.TIMEZONE
 
+
 @app.route("/", strict_slashes=False)
 def home():
     """Home page"""
     return render_template("3-index.html",
-                           home_title =_("Welcome to Holberton"),
+                           home_title=_("Welcome to Holberton"),
                            home_header=_("Hello world"))
+
 
 if __name__ == "__main__":
     HOST = "0.0.0.0"
