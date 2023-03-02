818314f01ca3c7ccf325e4c97e21175eda04b568
Nick Nyanjui <nicknyanjui@gmail.com>
2023-03-03 01:33:15 +0300

    Add 0-app.py

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Basic flask app"""
+
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def root():
+    """Root page"""
+    return render_template("0-index.html",
+                           title="Welcome to Holberton",
+                           header="Hello world")
+
+
+if __name__ == "__main__":
+    HOST = "0.0.0.0"
+    PORT = 5000
+    app.run(host=HOST, port=PORT)
